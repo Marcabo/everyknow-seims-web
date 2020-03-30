1609720cf9599d3962ca1b8aaa5c1cf373e241d1
wubo25320 <1790043847@qq.com>
2020-03-30 22:30:36 +0800

    集成 github-vue-admin-template

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import getters from './getters'
+import app from './modules/app'
+import settings from './modules/settings'
+import user from './modules/user'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+const store = new Vuex.Store({
   modules: {
-  }
+    app,
+    settings,
+    user
+  },
+  getters
 })
+
+export default store
